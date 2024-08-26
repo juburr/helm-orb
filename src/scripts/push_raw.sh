467f5b7e4f6a47973536c048686df0bde18e2ffa
Justin Burr <20321959+juburr@users.noreply.github.com>
2024-08-26 00:11:21 -0400

    feat: add additional safeguard to protect secrets (#10)
    Co-authored-by: Justin Burr <juburr@users.noreply.github.com>

--- src/scripts/push_raw.sh
+++ src/scripts/push_raw.sh
@@ -9,10 +9,31 @@ PASSWORD=${!PARAM_PASSWORD_ENV}
 UPLOAD_PATH=$(circleci env subst "${PARAM_UPLOAD_PATH}")
 USERNAME=${!PARAM_USERNAME_ENV}
 
+# Cleanup makes a best effort to destroy all secrets.
+cleanup_secrets() {
+    echo "Cleaning up secrets..."
+    unset PASSWORD
+    unset PARAM_PASSWORD_ENV
+    unset USERNAME
+    unset PARAM_USERNAME_ENV
+    echo "Secrets destroyed."
+}
+trap cleanup_secrets EXIT
+
 # Print parameters for debugging purposes
 echo "Running Helm raw push command..."
 echo "  CHART: ${CHART}"
 echo "  UPLOAD_PATH: ${UPLOAD_PATH}"
 
+# Require HTTPS to be used
+if [[ "${UPLOAD_PATH}" == https* ]]; then
+    echo "The UPLOAD_PATH starts with 'https'. Proceeding..."
+else
+    echo "The push_raw command currently requires the use the HTTPS."
+    echo "Credentials are included within request headers."
+    exit 1
+fi
+
 # Upload the Helm chart to a raw repository
-curl -u "${USERNAME}:${PASSWORD}" "${UPLOAD_PATH}" --upload-file "${CHART}" -v
+# Avoid verbose argument due to potential to leak headers; manually write out HTTP response code to debug instead.
+curl -u "${USERNAME}:${PASSWORD}" "${UPLOAD_PATH}" --upload-file "${CHART}" -w "Response code: %{http_code}\n"
--- src/scripts/registry_login.sh
+++ src/scripts/registry_login.sh
@@ -8,6 +8,17 @@ PASSWORD=${!PARAM_PASSWORD_ENV}
 REGISTRY=$(circleci env subst "${PARAM_REGISTRY}")
 USERNAME=${!PARAM_USERNAME_ENV}
 
+# Cleanup makes a best effort to destroy all secrets.
+cleanup_secrets() {
+    echo "Cleaning up secrets..."
+    unset PASSWORD
+    unset PARAM_PASSWORD_ENV
+    unset USERNAME
+    unset PARAM_USERNAME_ENV
+    echo "Secrets destroyed."
+}
+trap cleanup_secrets EXIT
+
 # Print parameters for debugging purposes
 echo "Running Helm registry login command..."
 echo "  REGISTRY: ${REGISTRY}"
